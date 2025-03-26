e7c069da1cc6d455bfb16f14a390b1e04649478f
shifat23 <shifat@revo-interactive.com>
2025-03-26 17:19:23 +0600

    lets try this time again

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header
             className="sticky top-0 z-50 bg-theme-dark px-4 py-4 flex items-center justify-between border-b border-amber-400 shadow shadow-amber-400 sm:hidden">
-            <div className="signature text-3xl">Sifat.dev again</div>
+            <div className="signature text-3xl">Sifat.dev again and again</div>
             <div>
                 <GiEgyptianProfile className={"text-4xl"}/>
             </div>
